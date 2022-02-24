91378cfbc565abd6bd39eb5a27867256d08f4c72
mio kataoka <mio.kanna@gmail.com>
2022-02-23 19:43:06 -0800

    commit tree d09fbb844e8a5d745ef4c2cde6eef239be49fda7

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+saturdayFun();
+function saturdayFun(doThis){
+    if (doThis != null) {
+    return `This Saturday, I want to ${doThis}!`;
+    } else {
+        return `This Saturday, I want to roller-skate!`;
+    };
+}
+const mondayWork = function(doThis) {
+    if (doThis != null) {
+    return `This Monday, I will ${doThis}.`
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+function wrapAdjective(str = `*`) {
+    return function (adj = `special`) {
+        return `You are ${str}${adj}${str}!`;
+        }
+    }
